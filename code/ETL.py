31bd7d698357fbed6252c55a4e44e14a51d26972
Catsofsuffering <821621930@qq.com>
2020-02-26 10:46:28 +0800

    finish dimensionless works

--- code/ETL.py
+++ code/ETL.py
@@ -1,7 +1,10 @@
 # coding: utf-8
 import pandas as pd
 import os
-from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import LabelEncoder
 
 def getPath():
     """
@@ -53,7 +56,8 @@ def dataConcat(dataMats):
         labelData = pd.concat([labelData, i], axis=1)
     return labelData
 
-def dataScale(dataMat):
+def dataStdScale(dataMat):
+
     """
     将数据标准化
 
@@ -62,5 +66,44 @@ def dataScale(dataMat):
     Returns:
         scaleData: 标准化后的数据矩阵
     """
-    scaleData = preprocessing.scale(dataMat)
+    scaleData = StandardScaler().fit_transform(dataMat)
     return scaleData
+
+def dataMMScale(dataMat):
+    
+    """
+    将数据最值缩放
+
+    Args:
+        dataMats: 未进行最值缩放处理的数据矩阵
+    Returns:
+        scaleData: 最值缩放后的数据矩阵
+    """
+    scaleData =  MinMaxScaler().fit_transform(dataMat)
+    return scaleData
+
+def onehotData(dataMat, colName):
+    """
+    将定性数据转换为哑编码
+
+    Args:
+        dataMat: 文本数据列表
+        colName: 需要转换的字段列名
+    Returns:a
+        onehotDatalist: 哑编码后的数据矩阵
+    """
+    onehotDatalist = LabelBinarizer().fit_transform(dataMat[colName])
+    return onehotDatalist
+
+def labencData(dataMat, colName):
+    """
+    将定性数据哑编码
+
+    Args:
+        dataMat: 文本数据列表
+        colName: 需要转换的字段列名
+    Returns:a
+        labencDatalist: 哑编码后的数据矩阵
+    """
+    labencDatalist = LabelEncoder().fit_transform(dataMat[colName])
+    return labencDatalist
